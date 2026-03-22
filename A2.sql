4cb0c1396261597a4ca99eec807c95e6f6ed81b1
JBTmarel <a.orribjarnason@gmail.com>
2026-03-22 12:12:52 +0000

    Query 1

--- A2.sql
+++ A2.sql
@@ -1,2 +1,11 @@
-SELECT * FROM  orku_einingar
-LIMIT 10
+SELECT eining_heiti power_plant_source,
+    EXTRACT(year from timi) AS year,
+    EXTRACT(month from timi) AS month,
+    tegund_maelingar measurement_type,
+    SUM(gildi_kwh) total_kwh
+FROM raforka_legacy.orku_maelingar
+GROUP BY eining_heiti, 
+    EXTRACT(month from timi),
+    EXTRACT(year from timi),
+    tegund_maelingar
+ORDER BY eining_heiti, month ASC, total_kwh DESC
