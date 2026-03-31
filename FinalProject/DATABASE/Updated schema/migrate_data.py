b9a098a348d4bf3c0bf3c15b7340c50ab0492637
kristjanaa23 <kristjanaa23@ru.is>
2026-03-31 14:59:05 +0000

    Task F

--- FinalProject/DATABASE/Updated schema/migrate_data.py
+++ FinalProject/DATABASE/Updated schema/migrate_data.py
@@ -4,7 +4,7 @@ import math
 
 # --- CONFIGURATION ---
 USER = "postgres"
-PASSWORD = "orri"  # Replace with your actual password
+PASSWORD = "221103"  # Replace with your actual password
 PORT = "5432"
 HOST = "localhost"
 DATABASE_NAME = "OrkuflaediIsland"
