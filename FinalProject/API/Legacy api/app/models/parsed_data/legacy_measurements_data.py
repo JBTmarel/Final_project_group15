d415720e8045662c4dc1abbe0c3dbc10d56c7b8b
kristjanaa23 <kristjanaa23@ru.is>
2026-03-31 21:17:17 +0000

    removing duplicate from legacy api

--- FinalProject/API/Legacy api/app/models/parsed_data/legacy_measurements_data.py
+++ FinalProject/API/Legacy api/app/models/parsed_data/legacy_measurements_data.py
@@ -1,13 +1,1 @@
 # Task E1 
-
-from dataclasses import dataclass
-from datetime import datetime
-
-@dataclass
-class MeasurementData:
-    eining_heiti: str
-    tegund_maelingar: str
-    sendandi_maelingar: str
-    timi: datetime
-    gildi_kwh: float
-    notandi_heiti: str | None
